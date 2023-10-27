38b532fc7edef18490829095072779783e1b41fe
RidoyMia <ridoymia153159hero@gmail.com>
2023-10-27 02:33:53 -0600

    seller dashboard locally working

--- src/app/seller/Layout.tsx
+++ src/app/seller/Layout.tsx
@@ -33,14 +33,11 @@ const layout = ({children} : {
       
     return (
         <div className='bg-gray-900 my-10 text-white  lg:container md:container px-2'>
-            <div className='grid grid-cols-4 lg:grid-cols-6  md:grid-cols-6 py-20'>
-            <div className='col-span-0 lg:col-span-1 md:col-span-1'>
-              <SellerHeader></SellerHeader>
-            </div>
-            <div className='col-span-3 lg:col-span-4 md:col-span-4 px-2 lg:px-10 md:px-8 '>
-                {children}
-            </div>
-            </div>
+           <UserHeader></UserHeader>
+           <h1>seller</h1>
+           {
+            children
+           }
         </div>
     );
 };
