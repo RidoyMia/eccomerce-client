1cc61b8c94eacdfb4d007c830fde75f820737507
RidoyMia <ridoymia153159hero@gmail.com>
2023-10-27 02:41:52 -0600

    seller layout page created

---
+++ src/app/(sellerLayout)/seller/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const layout = ({children}:any) => {
+    return (
+        <div>
+
+            <h1>seller</h1>
+            {children}
+        </div>
+    );
+};
+
+export default layout;
